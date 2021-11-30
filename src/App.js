74b4c24e680a22934b47843f77a4265a43748450
siddhartha8916 <siddhartha8916@gmail.com>
2021-11-30 16:40:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import AppNavbar from './components/Layout/AppNavbar';
+import {BrowserRouter as Router, Route,  Switch} from 'react-router-dom';
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Contact from './components/pages/Contact'
+import Appointment from './components/pages/Appointment'
+import Error404 from './components/pages/Error404'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>
+       <AppNavbar/>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About}/>
+            <Route exact path="/contact" component={Contact}/>
+            <Route exact path="/appointment" component={Appointment}/>
+            <Route component={Error404}/>
+            
+          </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Layout/AppNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppNavbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AppNavbar
